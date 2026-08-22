0f69109e9760e89bdcab2ac86e646c5db9ce74c9
sachinchaudhary4211-tech <sachinchaudhary4211@gmail.com>
2026-08-22 14:45:42 +0530

    Update forex_news.py

--- forex_news.py
+++ forex_news.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1540641249135165490/0yIDqzxhUMMDbt2sW0MeY27gtNMo0QNOzisEbFtz_PS9p3G2hgA36zs5ZtsfnM6YEbpt"
 
-FEED_URL = "https://www.forexfactory.com/calendar/rss"
+FEED_URL = "https://nfs.faireconomy.media/ff_calendar_thisweek.json"
+
 
 def send_to_discord(title, description):
     data = {
@@ -18,18 +19,44 @@ def send_to_discord(title, description):
         ]
     }
 
-    response = requests.post(WEBHOOK_URL, json=data)
-    print(response.status_code)
+    requests.post(WEBHOOK_URL, json=data)
 
 
 try:
-    response = requests.get(FEED_URL)
+    response = requests.get(
+        FEED_URL,
+        headers={"User-Agent": "Mozilla/5.0"},
+        timeout=30
+    )
+
     response.raise_for_status()
 
-    send_to_discord(
-        "📈 Forex Factory Update",
-        f"Forex Factory calendar checked successfully.\n\nTime: {datetime.now()}"
-    )
+    news = response.json()
+
+    high_impact = [
+        event for event in news
+        if event.get("impact") == "High"
+    ]
+
+    if high_impact:
+        event = high_impact[0]
+
+        message = (
+            f"**Country:** {event.get('country')}\n"
+            f"**Event:** {event.get('title')}\n"
+            f"**Time:** {event.get('date')}\n"
+            f"**Impact:** {event.get('impact')}\n"
+            f"**Forecast:** {event.get('forecast')}\n"
+            f"**Previous:** {event.get('previous')}"
+        )
+
+        send_to_discord("🔴 HIGH IMPACT FOREX NEWS", message)
+
+    else:
+        send_to_discord(
+            "📊 Forex News Update",
+            "No high-impact news found."
+        )
 
 except Exception as e:
     send_to_discord(

7d8905c6f107f4dd9375ab668fd6f18b555b519c
sachinchaudhary4211-tech <sachinchaudhary4211@gmail.com>
2026-08-22 20:10:04 +0530

    Update x_forex_news.py

--- x_forex_news.py
+++ x_forex_news.py
@@ -3,7 +3,7 @@ import json
 import hashlib
 import requests
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, Pillow
 from io import BytesIO
 from datetime import datetime, timezone
 
